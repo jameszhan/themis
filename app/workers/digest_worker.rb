3cdaded920d29a31c6900331fe3919d2384cd5bc
James Zhan <zhiqiangzhan@gmail.com>
2014-08-22 15:00:18 +0800

    add digest worker.

---
+++ app/workers/digest_worker.rb
@@ -0,0 +1,34 @@
+class DigestWorker
+  include Sidekiq::Worker
+
+  BUF_SIZE = 1024 * 1024 * 1024
+
+  def perform(id)
+    blob = Blob.find(id)
+    begin
+      if self.class.accept?(blob)
+        logger.info { "digest for \"#{blob.uri}\"" }
+        digest = digest(blob.uri)
+        blob.update(digest: digest)
+      end
+    rescue Exception => e
+      logger.error "path => #{blob.uri} error: #{e.message}"
+      logger.error e.backtrace
+    end
+  end
+
+  def digest(uri)
+    sha = Digest::SHA1.new
+    open(uri, 'rb') do|io|
+      while buf = io.read(BUF_SIZE)
+        sha.update(buf)
+      end
+    end
+    sha.hexdigest
+  end
+
+  def self.accept?(blob)
+    !blob.digest || File.stat(blob.uri).mtime != blob.modified_at
+  end
+
+end
